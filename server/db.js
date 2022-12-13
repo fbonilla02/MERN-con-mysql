58e62efde93a6870f41841677121ab233f8331bf
francisco bonilla <franciscobonillac@gmail.com>
2022-12-12 20:35:21 -0500

    agregue el dotenv

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
 import { createPool } from "mysql2/promise";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from "./config";
 
 export const pool = createPool({
-    host: 'localhost',
-    port: 3305,
-    user: 'root',
-    password: 'franpassword',
-    database:'taskdb'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database:DB_DATABASE,
 })
